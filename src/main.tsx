cfd87e6b05179eb377cfb783fc606dafa8b8f16e
juanftg <juanftg@gmail.com>
2026-09-05 18:14:48 -0700

    Touch yaw button styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RidgefoldApp } from "./game/RidgefoldApp";
 import "./styles.css";
+import "./yaw.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
